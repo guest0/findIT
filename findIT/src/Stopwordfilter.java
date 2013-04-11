f9e2165784534bb0592534c4912e9b1d47cf4210
Kündig <Kndig@Kndig-PC>
2013-04-11 21:28:03 +0200

    * funktionsfähiger stopword filter

--- findIT/src/Stopwordfilter.java
+++ findIT/src/Stopwordfilter.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 
 public class Stopwordfilter
@@ -6,25 +9,36 @@ public class Stopwordfilter
 	private static String stopwordDirectory = "stopwords";
 
 	public static String[] filterStopwords(String[] originalTokens) {
-		String[] filteredTokens	= new String[originalTokens.length];
-		File stopwordDir		= new File(stopwordDirectory);
+	    List<String> filteredTokens	= new LinkedList<String>();
+	    File stopwordDir	= new File(stopwordDirectory);
 
-		if (stopwordDir.isDirectory() && stopwordDir.exists()) {
-			File[] files = stopwordDir.listFiles();
-				
-				/////////////////////////////////////////////////////////////////////////////////
-				//////							KÜNDIGS CODE HERE						/////////
-				//////																	/////////
-				//////						BITTE EINBAUEN (WICHTIG):					/////////
-				//////	String currentToken = originalTokens[i].trim().toLowerCase();	/////////
-				//////																	/////////
-				/////////////////////////////////////////////////////////////////////////////////
-			
-			return filteredTokens;
-		} else {
-			System.out.println("STOPWORD-Verzeichnis nicht gefunden. Bitte Verzeichnis korrekt angeben!");
-			return null;
+	    if (stopwordDir.isDirectory() && stopwordDir.exists()) {
+	        File[] files = stopwordDir.listFiles();
+		String filename	= files[0].getAbsolutePath();
+	        String[] stopwords	= splitFileStringIntoTokens(Utilities.readFile(filename));
+	        for (String token : originalTokens) {
+		    boolean isStopword	= false;
+		    for (String stopword : stopwords) {
+		        token	= token.trim().toLowerCase();
+		        stopword	= stopword.trim().toLowerCase();
+		        if (token.equals(stopword)) {
+			    isStopword	= true;
+			}
+		    }
+		    if (isStopword == false) {
+			filteredTokens.add(token);
+		    }
 		}
+		return filteredTokens.toArray(new String[0]);
+	    } else {
+		System.out.println("STOPWORD-Verzeichnis nicht gefunden. Bitte Verzeichnis korrekt angeben!");
+		return null;
+	    }
+	}
+	
+	private static String[] splitFileStringIntoTokens(String fileContent) {
+	    Pattern p	= Pattern.compile("[\\W]+");
+	    return p.split(fileContent);
 	}
 
 }
